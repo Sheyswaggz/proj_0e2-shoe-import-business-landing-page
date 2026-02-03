45d131df8cc24e5dffb722257b5a50b660893c45
Sheyswaggz <oluwaseun65771@gmail.com>
2026-02-03 01:40:19 +0100

    Auto-merge: Setup Project Foundation and Development Environment
    Automatically merged PR for task f95f087f-b69e-4698-aa06-61a66ed7a37e

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-undef': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'all'],
+    'no-alert': 'warn',
+    'no-debugger': 'warn',
+  },
+  ignorePatterns: ['dist/', 'build/', 'node_modules/', '*.config.js'],
+};
